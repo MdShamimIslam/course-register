874e735e0e8236067aaf74e667128376d9896ca3
MdShamimIslam <shamim401897@gmail.com>
2023-11-01 15:37:40 +0600

    credit hour and price work finished and code updated

--- src/components/CourseBox/CourseBox.jsx
+++ src/components/CourseBox/CourseBox.jsx
@@ -1,12 +1,12 @@
-
 import CourseName from "../CourseName/CourseName";
 
 const CourseBox = ({ hour, price, courseName }) => {
   const remainingHr = 20 - hour;
   return (
     <div className="lg:w-1/4 w-2/3 mx-auto bg-slate-300 h-[485px] p-4 lg:ml-6 ml-16 mt-4 rounded-lg">
-      <h2 className="text-xl font-semibold text-blue-600">
-        Create our remaining <span className="text-red-600">{remainingHr}</span> hr
+      <h2 className="text-xl font-semibold">
+        Create our remaining <span className="text-red-600">{remainingHr}</span>{" "}
+        hours
       </h2>
       <br />
       <hr />
@@ -17,10 +17,18 @@ const CourseBox = ({ hour, price, courseName }) => {
       ))}
 
       <hr />
-      <h4 className="font-semibold mt-6">Total credit hours : <span className="text-purple-600">{hour}</span></h4>
+
+      <h4 className="font-semibold mt-6">
+        Total credit hours :
+        {hour ? <span className="text-purple-600"> {hour}</span> : ""}
+      </h4>
+
       <br />
       <hr />
-      <h4 className="font-semibold mt-4">Total Price : <span className="text-purple-600">{price}</span> USD</h4>
+      <h4 className="font-semibold mt-4">
+        Total Price :
+        {price ? <span className="text-purple-600"> {price} USD</span> : ""}
+      </h4>
     </div>
   );
 };
